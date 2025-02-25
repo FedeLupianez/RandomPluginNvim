9221103da52d61fac91c15faa412d848f976571f
Fede Lupianez <lupianezfederico@gmail.com>
2025-02-25 12:15:01 -0300

    new command for custom format line

--- lua/RandomPlugin/commands.lua
+++ lua/RandomPlugin/commands.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local Functions = require("RandomPlugin.functions")
+local defaults = require("RandomPlugin.config").options.defaults
 
 function M.setup()
 	vim.api.nvim_create_user_command("RandomNumber", function()
@@ -14,6 +15,12 @@ function M.setup()
 		local length = vim.fn.input("Longitud : ")
 		local result = Functions.generateRandomString(length)
 		vim.api.nvim_put({ result }, "c", true, true)
+	end, {})
+
+	vim.api.nvim_create_user_command("RandomFormat", function()
+		local format = vim.fn.input("Formato : ")
+		local result = Functions.formatString(format, defaults.min_number, defaults.max_number, defaults.length_string)
+		vim.api.nvim_put({ result }, "l", true, true)
 	end, {})
 end
 
--- lua/RandomPlugin/functions.lua
+++ lua/RandomPlugin/functions.lua
@@ -14,4 +14,43 @@ function M.generateRandomString(lenght)
 	return str
 end
 
+function M.formatString(input, default_min, default_max, default_lenght)
+	local result = {}
+
+	for column in string.gmatch(input, "([^,]+)") do -- Separo las columnas
+		local key, value = string.match(column, "([^=]+)=([^=]+)")
+		if key == "s" then
+			local lenght = tonumber(value)
+			if lenght then
+				table.insert(result, M.generateRandomString(lenght))
+			else
+				table.insert(result, M.generateRandomString(default_lenght))
+			end
+		elseif key == "n" then
+			local min_max_table = {}
+			local count = 0
+			for val in string.gmatch(value, "([^/]+)") do
+				local num = tonumber(val)
+				if num then
+					table.insert(min_max_table, tonumber(val))
+				else
+					if count == 0 then
+						table.insert(min_max_table, default_min)
+						count = count + 1
+					else
+						table.insert(min_max_table, default_max)
+						count = 0
+					end
+				end
+			end
+
+			if #min_max_table == 2 then
+				local number = M.generateRandomNumber(min_max_table[1], min_max_table[2])
+				table.insert(result, number)
+			end
+		end
+	end
+	return table.concat(result, ",")
+end
+
 return M
