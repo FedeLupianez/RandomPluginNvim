77be6d262c9594bdf8de1edca0814b80d603149c
Fede Lupianez <lupianezfederico@gmail.com>
2025-02-25 12:26:06 -0300

    Me aseguro de que el buffer es modificable antes de escribir e hice un fix para el command de formato personalizado

--- lua/RandomPlugin/commands.lua
+++ lua/RandomPlugin/commands.lua
@@ -8,19 +8,34 @@ function M.setup()
 		local min = vim.fn.input("min : ")
 		local max = vim.fn.input("max : ")
 		local result = Functions.generateRandomNumber(min, max)
-		vim.api.nvim_put({ tostring(result) }, "c", true, true)
+
+		if vim.bo.modifiable then
+			vim.api.nvim_put({ tostring(result) }, "c", true, true)
+		else
+			vim.notify(tostring(result))
+		end
 	end, {})
 
 	vim.api.nvim_create_user_command("RandomString", function()
 		local length = vim.fn.input("Longitud : ")
 		local result = Functions.generateRandomString(length)
-		vim.api.nvim_put({ result }, "c", true, true)
+
+		if vim.bo.modifiable then
+			vim.api.nvim_put({ result }, "c", true, true)
+		else
+			vim.notify(result)
+		end
 	end, {})
 
 	vim.api.nvim_create_user_command("RandomFormat", function()
 		local format = vim.fn.input("Formato : ")
 		local result = Functions.formatString(format, defaults.min_number, defaults.max_number, defaults.length_string)
-		vim.api.nvim_put({ result }, "l", true, true)
+
+		if vim.bo.modifiable then
+			vim.api.nvim_put({ result }, "l", true, true)
+		else
+			vim.notify(result)
+		end
 	end, {})
 end
 
--- lua/RandomPlugin/mappings.lua
+++ lua/RandomPlugin/mappings.lua
@@ -7,13 +7,25 @@ function M.setup()
 
 	vim.keymap.set("n", "<leader>rn", function()
 		local number = Functions.generateRandomNumber(config.defaults.min_number, config.defaults.max_number)
-		vim.api.nvim_put({ tostring(number) }, "c", true, true)
+
+		if vim.bo.modifiable then
+			vim.api.nvim_put({ tostring(number) }, "c", true, true)
+		else
+			vim.notify(tostring(number))
+		end
 	end, { desc = "Generate Random number" })
 
 	vim.keymap.set("n", "<leader>rs", function()
 		local str = Functions.generateRandomString(config.defaults.length_string)
-		vim.api.nvim_put({ str }, "c", true, true)
+
+		if vim.bo.modifiable then
+			vim.api.nvim_put({ str }, "c", true, true)
+		else
+			vim.notify(str)
+		end
 	end, { desc = "Generate Random string" })
+
+	vim.keymap.set("n", "<leader>rf", "<CMD>RandomFormat<CR>")
 end
 
 return M
