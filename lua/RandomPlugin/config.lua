02a5c8dff1e317160cb6a4be099ceef07d9a08e4
Fede Lupianez <lupianezfederico@gmail.com>
2025-02-26 20:09:05 -0300

    Add custom keymaps option

--- lua/RandomPlugin/config.lua
+++ lua/RandomPlugin/config.lua
@@ -6,11 +6,21 @@ M.options = {
 		max_number = 999,
 		length_string = 14,
 	},
+
+	keymaps = {
+		random_number = "<leader>rn",
+		random_string = "<leader>rs",
+		random_format = "<leader>rf",
+	},
 }
 
 function M.setup(opts)
 	if next(opts) ~= nil then
-		M.options = opts
+		for key, value in pairs(opts) do
+			for key_2, value_2 in pairs(opts[key]) do
+				M.options[key][key_2] = value_2
+			end
+		end
 	end
 end
 
--- lua/RandomPlugin/mappings.lua
+++ lua/RandomPlugin/mappings.lua
@@ -12,18 +12,19 @@ end
 
 function M.setup()
 	local config = require("RandomPlugin.config").options
+	local keymaps = require("RandomPlugin.config").options.keymaps
 
-	vim.keymap.set("n", "<leader>rn", function()
+	vim.keymap.set("n", keymaps.random_number, function()
 		local number = Functions.generateRandomNumber(config.defaults.min_number, config.defaults.max_number)
 		put_to_buffer(tostring(number))
 	end, { desc = "Generate Random number" })
 
-	vim.keymap.set("n", "<leader>rs", function()
+	vim.keymap.set("n", keymaps.random_string, function()
 		local str = Functions.generateRandomString(config.defaults.length_string)
 		put_to_buffer(str)
 	end, { desc = "Generate Random string" })
 
-	vim.keymap.set("n", "<leader>rf", "<CMD>RandomFormat<CR>", { desc = "Custom Format" })
+	vim.keymap.set("n", keymaps.random_format, "<CMD>RandomFormat<CR>", { desc = "Custom Format" })
 end
 
 return M
